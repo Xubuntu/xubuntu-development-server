df86c5ce92047456fd91e53621337798845af44c
Pasi Lallinaho <pasi@shimmerproject.org>
2022-03-13 14:07:24 +0200

    Remove old version of "wallpaper-contest" WordPress plugin.
    New versions of the plugin will be tracked in a separate repository:
    https://github.com/Xubuntu/wallpaper-contest

--- contest.xubuntu.org/htdocs/wp-content/plugins/xubuntu-wallpaper-contest/confirm.js
+++
@@ -1,8 +0,0 @@
-jQuery( function( ) {
-	jQuery( 'a.delete-submission' ).click( function( e ) {
-		var confirm_delete = window.confirm( 'Are you sure you want to delete the submission?' );
-		if( confirm_delete != true ) {
-			e.preventDefault( );
-		}
-	} );
-} );
--- contest.xubuntu.org/htdocs/wp-content/plugins/xubuntu-wallpaper-contest/media.js
+++
@@ -1,5 +0,0 @@
-jQuery( function( ) {
-	jQuery.featherlight.defaults.closeOnClick = 'anywhere';
-	jQuery.featherlight.defaults.closeIcon = '';
-	jQuery.featherlight.defaults.loading = 'Loading...';
-} );
